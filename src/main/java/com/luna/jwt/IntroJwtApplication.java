3ee833455631c8c8d1f8d79eedea39b70f6b328f
Bruno Luna <luna.brsilva@gmail.com>
2021-12-11 16:36:40 -0300

    Geração do token-JWT

--- src/main/java/com/luna/jwt/IntroJwtApplication.java
+++ src/main/java/com/luna/jwt/IntroJwtApplication.java
@@ -1,13 +1,20 @@
 package com.luna.jwt;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Date;
 
-@SpringBootApplication
+import com.luna.jwt.jwt.TokenJwt;
+import com.luna.jwt.utils.Utils;
+
 public class IntroJwtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IntroJwtApplication.class, args);
+		public static void main(String args[]) throws InterruptedException {
+	        TokenJwt token = new TokenJwt(Utils.gerarChave());
+
+	        System.out.println("\t*** Gerando o JSON Web Token ***");
+	        Date dataDeExpiracao = Utils.definirDataDeExpiracao(60L); //60minutos para expirar o token
+	        String jwt = token.gerarToken("Bruno Luna", dataDeExpiracao);
+
+	        System.out.println("\nJWT: " + jwt);
 	}
 
 }
