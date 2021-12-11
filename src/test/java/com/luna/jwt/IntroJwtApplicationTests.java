8c36059de28f04a77131ff084f46fa0a975020a5
Bruno Luna <luna.brsilva@gmail.com>
2021-12-11 15:28:58 -0300

    Commit inicial

---
+++ src/test/java/com/luna/jwt/IntroJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luna.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
