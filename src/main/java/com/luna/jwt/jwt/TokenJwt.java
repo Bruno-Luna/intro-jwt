5735b09a341ac1d3bf77e501fbf77e00aa81be36
Bruno Luna <luna.brsilva@gmail.com>
2021-12-11 16:34:35 -0300

    Package Jwt e classe TokenJwt

---
+++ src/main/java/com/luna/jwt/jwt/TokenJwt.java
@@ -0,0 +1,31 @@
+package com.luna.jwt.jwt;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.security.Key;
+import java.util.Date;
+
+public class TokenJwt {
+
+	private Key chave;
+	
+	private String jwt;
+	
+	public TokenJwt(Key chave) {
+		this.chave = chave;
+	}
+	
+	 public String gerarToken(String nomeUsuario, Date dataExpiracao) {
+	        jwt = Jwts.builder()
+	                .setHeaderParam("typ","JWT")
+	                .setSubject(nomeUsuario)
+	                .setIssuer("Java developer")
+	                .setIssuedAt(new Date())
+	                .setExpiration(dataExpiracao)
+	                .signWith(SignatureAlgorithm.HS256, chave)
+	                .compact();
+	        return jwt;
+	    }
+}
